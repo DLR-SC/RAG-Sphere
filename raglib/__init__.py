446d682443020e9455926d23cbccb73b6c58c307
Diepenbruck <diep_ti@sc-030200l.intra.dlr.de>
2025-12-17 11:26:03 +0100

    add packaging

--- raglib/__init__.py
+++ raglib/__init__.py
@@ -1,6 +1,7 @@
 """The RAGlib package."""
-from .rag import RAG
 
-__all__ = [
-    RAG
-] 
+from indexer_engine import IndexerEngine
+from query_engine import QueryEngine
+from rag import RAG
+
+__all__ = ["IndexerEngine", "QueryEngine", "RAG"]
